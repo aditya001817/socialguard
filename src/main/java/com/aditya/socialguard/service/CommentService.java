b460f46a639ffab80d71a0795b42aadf347bad29
aditya001817 <aditya.chauhan00018@gmail.com>
2026-04-24 10:38:42 +0530

    updated comment controller

--- src/main/java/com/aditya/socialguard/service/CommentService.java
+++ src/main/java/com/aditya/socialguard/service/CommentService.java
@@ -12,8 +12,29 @@ import java.time.LocalDateTime;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final RedisService redisService;
 
     public Comment addComment(Comment comment) {
+
+    if (comment.getDepthLevel() > 20) {
+            throw new RuntimeException("Depth limit exceeded");
+        }
+
+        boolean isBot = comment.getAuthorId() > 1000;
+
+        if (isBot) {
+
+            Long count = redisService.incrementBotCount(comment.getPostId());
+            if (count > 100) {
+                throw new RuntimeException("Too many bot replies");
+            }
+
+            redisService.incrementVirality(comment.getPostId(), 1);
+
+        } else {
+            redisService.incrementVirality(comment.getPostId(), 50);
+        }
+
         comment.setCreatedAt(LocalDateTime.now());
         return commentRepository.save(comment);
     }
